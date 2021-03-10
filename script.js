6d44150c32d11e70f270cfc383ededc538ae1164
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2021-03-10 16:34:23 +0700

    update version 2

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+let clickedCounts = 0;
+let switchAfter = Math.floor(Math.random() * 5) + 1;
+
 window.onload = () => {
   const search = new URLSearchParams(window.location.search);
   const m = search.get("m");
   const s = search.get("ok") || "HIHI";
+  const okeText = search.get("ok_text") || "Có";
+  const noText = search.get("no_text") || "Không";
   const text = document.querySelector("#text");
   const btnOk = document.querySelector("#btn-ok"),
     btnNo = document.querySelector("#btn-no");
+
+  console.log({ btnOk, btnNo });
 
   const randomEffect = () => {
     const effects = [
@@ -26,6 +33,9 @@ window.onload = () => {
 
   animateCSS(text, randomEffect());
   text.innerHTML = m;
+  btnOk.value = okeText;
+  btnNo.value = noText;
+
   btnOk.addEventListener("click", function () {
     text.innerHTML = s;
     text.style["font-size"] = "128px";
@@ -36,14 +46,35 @@ window.onload = () => {
     };
     loop();
   });
+
   btnNo.addEventListener("click", function () {
     btnNo.style.position = "fixed";
-    btnNo.style.top =
-      Math.max(Math.random() * window.innerHeight - btnNo.clientHeight, 0) +
-      "px";
-    btnNo.style.left =
-      Math.max(Math.random() * window.innerWidth - btnNo.clientWidth, 0) + "px";
+    clickedCounts++;
 
+    console.log(switchAfter, clickedCounts);
+    if (switchAfter === clickedCounts) {
+      const top = btnNo.getBoundingClientRect().top,
+        left = btnNo.getBoundingClientRect().left;
+
+      console.log(top, left);
+
+      btnNo.style.top = btnOk.getBoundingClientRect().top + "px";
+      btnNo.style.left = btnOk.getBoundingClientRect().left + "px";
+
+      btnOk.style.position = "fixed";
+      btnOk.style.top = top + "px";
+      btnOk.style.left = left + "px";
+
+      clickedCounts = 0;
+      switchAfter = Math.floor(Math.random() * 5) + 1;
+    } else {
+      btnNo.style.top =
+        Math.max(Math.random() * window.innerHeight - btnNo.clientHeight, 0) +
+        "px";
+      btnNo.style.left =
+        Math.max(Math.random() * window.innerWidth - btnNo.clientWidth, 0) +
+        "px";
+    }
     animateCSS(btnNo, randomEffect());
   });
 };
